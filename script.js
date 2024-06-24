142787a828495ee045d075b44e9a9669b1861b74
ericluec <eric.luecking@gmail.com>
2024-06-24 16:02:53 +0200

    new features

--- script.js
+++ script.js
@@ -1,23 +1,60 @@
 const search = document.querySelector(".search-box input"),
-      images = document.querySelectorAll(".image-box");
+      images = document.querySelectorAll(".image-box"),
+      modal = document.getElementById("modal"),
+      modalImg = document.getElementById("modal-img"),
+      span = document.getElementsByClassName("close")[0];
 
-search.addEventListener("keyup", e =>{
-    if(e.key == "Enter"){
-        let searcValue = search.value,
-            value = searcValue.toLowerCase();
-            images.forEach(image =>{
-                if(value === image.dataset.name){ //matching value with getting attribute of images
-                    return image.style.display = "block";
-                }
+search.addEventListener("keyup", e => {
+    if (e.key === "Enter") {
+        let searchValue = search.value.toLowerCase();
+        images.forEach(image => {
+            if (image.dataset.name === searchValue) {
+                image.style.display = "block";
+            } else {
                 image.style.display = "none";
-            });
+            }
+        });
     }
 });
 
-search.addEventListener("keyup", () =>{
-    if(search.value != "") return;
-
-    images.forEach(image =>{
+search.addEventListener("keyup", () => {
+    if (search.value !== "") return;
+    images.forEach(image => {
         image.style.display = "block";
-    })
-})
+    });
+});
+
+images.forEach(image => {
+    image.querySelector('img').addEventListener('click', () => {
+        modal.style.display = "block";
+        modalImg.src = image.querySelector('img').src;
+    });
+
+    image.querySelector('.download-png-btn').addEventListener('click', () => {
+        const link = document.createElement('a');
+        link.href = image.querySelector('img').src.replace(/\.cr2$/, '.png'); // Assuming the PNG version has the same name but different extension
+        link.download = `${image.querySelector('img').alt}.png`;
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    });
+
+    image.querySelector('.download-cr2-btn').addEventListener('click', () => {
+        const link = document.createElement('a');
+        link.href = image.querySelector('img').src.replace(/\.png$/, '.cr2'); // Assuming the CR2 version has the same name but different extension
+        link.download = `${image.querySelector('img').alt}.cr2`;
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    });
+});
+
+span.onclick = function() {
+    modal.style.display = "none";
+};
+
+window.onclick = function(event) {
+    if (event.target === modal) {
+        modal.style.display = "none";
+    }
+};
