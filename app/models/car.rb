fcbd197b3ec965e5adb278807e37d082c58ee4d3
mikeezvz <mike.zogheib@lernende.bbw.ch>
2024-07-13 22:06:00 +0200

    feat: Delete button on cards
    index.html.erb was updated and now every card has a delete button. The trash.svg icon was styled in script.js to make it look better.

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,4 +1,9 @@
 class Car < ApplicationRecord
     has_one_attached :image
+
+  validates :brand, presence: true
+  validates :model, presence: true
+  validates :likes, presence: true
+  validates :image, presence: { message: 'Image must be attached' }
   end
   
